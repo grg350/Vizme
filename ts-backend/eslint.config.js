b6c7b1830dd935c1c2114b4c3bd448bfffb434f4
manojrai200 <manojrai9801@gmail.com>
2026-01-26 15:51:17 +0545

    lint: enable type-aware ESLint parsing with tsconfig root

--- ts-backend/eslint.config.js
+++ ts-backend/eslint.config.js
@@ -1,12 +1,21 @@
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default [
   {
     files: ["**/*.ts"],
     languageOptions: {
       parser: tsparser,
-      parserOptions: { sourceType: "module" },
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
     },
     plugins: { "@typescript-eslint": tseslint },
     rules: {
